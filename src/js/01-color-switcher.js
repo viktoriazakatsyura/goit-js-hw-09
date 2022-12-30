0b2512de1421b1f8c3860b89674071b46936b1f7
Viktoriazakatsyura <viktoriazakatsyura@gmail.com>
2022-12-31 01:41:40 +0200

    color-switcher done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startRandomColor = document.querySelector('button[data-start]');
+const stopRandomColor = document.querySelector('button[data-stop]');
 
+startRandomColor.addEventListener('click', onStart);
+stopRandomColor.addEventListener('click', onStop);
+
+function onStart() {
+  startRandomColor.setAttribute('disabled', 'true');
+  stopRandomColor.removeAttribute('disabled');
+
+  setTimer = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+}
+
+function onStop() {
+  startRandomColor.setAttribute('disabled', 'true');
+  stopRandomColor.removeAttribute('disabled');
+  clearInterval(setTimer);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
