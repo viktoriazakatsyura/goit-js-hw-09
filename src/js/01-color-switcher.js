eaaaf003905714294a04d45944f42edabe6ea6eb
Viktoriazakatsyura <viktoriazakatsyura@gmail.com>
2022-12-31 17:24:36 +0200

    правки 1з

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,24 +1,63 @@
 const startRandomColor = document.querySelector('button[data-start]');
 const stopRandomColor = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
 
-startRandomColor.addEventListener('click', onStart);
-stopRandomColor.addEventListener('click', onStop);
+stopRandomColor.disabled = true;
+let intervalId = null;
 
-function onStart() {
-  startRandomColor.setAttribute('disabled', 'true');
-  stopRandomColor.removeAttribute('disabled');
+const showRandomColor = {
+  DELAY: 1000,
 
-  setTimer = setInterval(() => {
-    document.body.style.background = getRandomHexColor();
-  }, 1000);
+  getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  },
+
+  interval() {
+    intervalId = setInterval(() => {
+      changeRandomHexColor();
+    }, this.DELAY);
+    stopRandomColor.disabled = false;
+  },
+  start() {
+    startRandomColor.addEventListener('click', () => {
+      this.interval();
+      startRandomColor.disabled = true;
+      stopRandomColor.disabled = false;
+    });
+    stopRandomColor.addEventListener('click', this.stop);
+  },
+
+  stop() {
+    clearInterval(intervalId);
+    startRandomColor.disabled = false;
+    stopRandomColor.disabled = true;
+  },
+};
+
+function changeRandomHexColor() {
+  bodyEl.style.backgroundColor = `${showRandomColor.getRandomHexColor()}`;
 }
 
-function onStop() {
-  startRandomColor.setAttribute('disabled', 'true');
-  stopRandomColor.removeAttribute('disabled');
-  clearInterval(setTimer);
-}
+showRandomColor.start();
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+// startRandomColor.addEventListener('click', onStart);
+// stopRandomColor.addEventListener('click', onStop);
+
+// function onStart() {
+//   startRandomColor.setAttribute('disabled', 'true');
+//   stopRandomColor.removeAttribute('disabled');
+
+//   setTimer = setInterval(() => {
+//     document.body.style.background = getRandomHexColor();
+//   }, 1000);
+// }
+
+// function onStop() {
+//   startRandomColor.setAttribute('disabled', 'true');
+//   stopRandomColor.removeAttribute('disabled');
+//   clearInterval(setTimer);
+// }
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
