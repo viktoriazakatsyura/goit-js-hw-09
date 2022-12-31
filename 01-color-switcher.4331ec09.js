dd45a685525fd2f942b2548fa0335c4042c815f7
viktoriazakatsyura <109316794+viktoriazakatsyura@users.noreply.github.com>
2022-12-31 15:25:07 +0000

    Deploying to gh-pages from @ viktoriazakatsyura/goit-js-hw-09@eaaaf003905714294a04d45944f42edabe6ea6eb 🚀

--- 01-color-switcher.4331ec09.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){t.setAttribute("disabled","true"),e.removeAttribute("disabled"),setTimer=setInterval((()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(function(){t.setAttribute("disabled","true"),e.removeAttribute("disabled"),clearInterval(setTimer)}));
-//# sourceMappingURL=01-color-switcher.4331ec09.js.map
